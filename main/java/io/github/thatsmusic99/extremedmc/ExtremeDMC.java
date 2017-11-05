146b6786879775ce6ef9d5f9dabe8cc842ab488a
Thatsmusic99 <tmtherobotcat@gmail.com>
2017-11-05 18:43:31 +0000

    Um

--- main/java/io/github/thatsmusic99/extremedmc/ExtremeDMC.java
+++ main/java/io/github/thatsmusic99/extremedmc/ExtremeDMC.java
@@ -45,6 +45,8 @@ public class ExtremeDMC extends JavaPlugin {
     public static Permission perms = null;
     public static ExtremeDMC instance;
 
+    // Ping
+
     @Override
     public void onEnable() {
         try {
